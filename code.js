ef866bf30fb1961cf673884fb266567e7e730fc0
Zenith4444 <100297389+Zenith4444@users.noreply.github.com>
2022-09-19 00:09:19 +0700

    code.js
    fixed some

--- code.js
+++ code.js
@@ -5,11 +5,12 @@ description = "desc"
 bot = commands.Bot(command_prefix='?', description=description)
 #startup command
 @bot.event
+
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
+    print('Loading...')
 #category of main commands
 class Main_Commands():
     def __init__(self, bot):
@@ -18,5 +19,6 @@ class Main_Commands():
 @bot.command(pass_context=True)
 async def ping(ctx):
     await bot.say("pong")
+
 #RunBot
 bot.run("2dFh5a35bFhk391diIen9e")
